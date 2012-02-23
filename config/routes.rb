91963b50235ea1374c215c60e64676704988203c
Kases <haghighi1989@yahoo.com>
2012-02-23 23:24:02 +0330

    Changed Index File

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Site2::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +51,7 @@ Site2::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+root :to => 'home#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
